15c3f617c517baffdd01763cc892ce3641c0b7cc
Olena Surilova <elen21surilova@gmail.com>
2026-08-07 21:49:57 +0200

    fix: clear structure of project

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function theme_files() {
-   wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600&family=Roboto:wght@400;500;700&display=swap');
-  // wp_enqueue_style('extra_styles', get_theme_file_uri( '/build/index.css' ));
-  wp_enqueue_style('main_styles', get_theme_file_uri( '/build/style-index.css' ));
-}
-
-add_action('wp_enqueue_scripts', 'theme_files');
